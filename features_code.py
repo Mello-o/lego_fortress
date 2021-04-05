57b8233f504bedc368f6d394eaef75ecb38605d0
Matheus Mello <matheusmellocunha@gmail.com>
2021-04-04 22:50:46 -0300

    Adding a buzzer to make a sound after the lights

--- features_code.py
+++ features_code.py
@@ -1,13 +1,16 @@
-from gpiozero import LED
+from gpiozero import LED, Buzzer
 from time import sleep
 
 led_positions = [17, 27, 22]
 leds = [LED(l) for l in led_positions]
+buzzer = Buzzer(23)
 
 while True:
     
     for l in leds:
         l.on()
+        buzzer.beep(on_time = 0.1)
         sleep(0.1)
         l.off()
         sleep(0.1)
+        
