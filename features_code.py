378e11946fb7747cafc80ff96ff481badf92046a
Matheus Mello <matheusmellocunha@gmail.com>
2021-04-11 01:16:58 -0300

    Adding a light sensor

--- features_code.py
+++ features_code.py
@@ -1,16 +1,18 @@
-from gpiozero import LED, Buzzer
+from gpiozero import LED, Buzzer, LightSensor
 from time import sleep
 
 led_positions = [17, 27, 22]
 leds = [LED(l) for l in led_positions]
 buzzer = Buzzer(23)
+ldr = LightSensor(21)
 
 while True:
-    
-    for l in leds:
-        l.on()
-        buzzer.beep(on_time = 0.1)
-        sleep(0.1)
-        l.off()
-        sleep(0.1)
-        
+    print(ldr.value)
+    if ldr.value < 0.1:
+       for l in leds:
+           l.on()
+           buzzer.beep(on_time = 0.1)
+           sleep(0.1)
+           l.off()
+           sleep(0.1)
+           buzzer.off()
